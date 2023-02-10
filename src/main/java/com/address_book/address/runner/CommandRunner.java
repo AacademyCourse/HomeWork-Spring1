3eee42639b98cce3442824e214b5a4f698301b26
Dimitar Enev <dreindead@abv.bg>
2023-02-10 20:13:52 +0200

    second commit

--- src/main/java/com/address_book/address/runner/CommandRunner.java
+++ src/main/java/com/address_book/address/runner/CommandRunner.java
@@ -3,7 +3,7 @@ package com.address_book.address.runner;
 import com.address_book.address.entity.Address;
 import com.address_book.address.entity.User;
 import com.address_book.address.repository.AddressRepository;
-import com.address_book.address.repository.UserRepository;
+import com.address_book.address.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,9 +15,9 @@ import java.util.List;
 @Component
 public class CommandRunner implements CommandLineRunner {
     @Autowired
-    UserRepository userRepository;
+    private UserService userService;
     @Autowired
-    AddressRepository addressRepository;
+    private AddressRepository addressRepository;
     @Override
     public void run(String... args) throws Exception {
         createUsers();
@@ -29,30 +29,32 @@ public class CommandRunner implements CommandLineRunner {
         dimitar.setLastName("Enev");
         dimitar.setPhoneNumber("0879385550");
         dimitar.setEmail("dreindead@abv.bg");
-        dimitar.setCreatedAt(Instant.now(Clock.systemUTC()));
+        dimitar.setCreatedAt(Instant.parse("1992-06-05T15:30:24.00Z"));
+        userService.addUser(dimitar);
 
         User ivan = new User();
         ivan.setFirstName("Ivan");
         ivan.setLastName("Malchev");
         ivan.setPhoneNumber("08777777777");
         ivan.setEmail("abv@abv.bg");
-        ivan.setCreatedAt(Instant.now(Clock.systemUTC()));
+        ivan.setCreatedAt(Instant.parse("1995-07-15T12:35:02.00Z"));
+        userService.addUser(ivan);
 
         User petar = new User();
         petar.setFirstName("Petar");
         petar.setLastName("Halvadzhiev");
         petar.setPhoneNumber("0888888888");
         petar.setEmail("bv@abv.bg");
-        petar.setCreatedAt(Instant.now(Clock.systemUTC()));
+        petar.setCreatedAt(Instant.parse("2002-01-05T18:12:34.00Z"));
+        userService.addUser(petar);
 
         User mladen = new User();
         mladen.setFirstName("Mladen");
         mladen.setLastName("Karagiozov");
         mladen.setPhoneNumber("0999999999");
         mladen.setEmail("v@abv.bg");
-        mladen.setCreatedAt(Instant.now(Clock.systemUTC()));
-
-        userRepository.saveAll(List.of(dimitar, ivan, petar, mladen));
+        mladen.setCreatedAt(Instant.parse("2000-09-25T08:42:14.00Z"));
+        userService.addUser(mladen);
     }
 
     public void createAddresses() {
